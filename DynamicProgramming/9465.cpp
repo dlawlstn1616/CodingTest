019e58d3318b4d8a6560ecea54be18cfc431b805
DDU <a01047181185@gmail.com>
2024-09-22 17:27:23 +0900

    DP 9465.cpp create

--- DynamicProgramming/9465.cpp
+++ DynamicProgramming/9465.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int n, int stk[2][100000];
+
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        cin >> n;
+        for(int j=0; j<2; j++){
+            for(int k=0; k<n; k++){
+                cin >> stk[j][k];
+            }
+        }
+    }
 
     
 }
